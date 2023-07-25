b541e75f7f0b056288aea51d78517ae9fafd1a41
belyivadim <belyivadim01@gmail.com>
2023-07-25 18:37:45 +0300

    implement endpoints, should pass the tests

--- src/app.js
+++ src/app.js
@@ -2,11 +2,30 @@ const express = require('express')
 
 const app = express()
 
+const users = [ 'Admin', 'Moder', 'User' ]
+
 app.get('/api', (req, res) => {
   res.status(200).json({
     message: 'OK'
   })
 })
+
+app.get('/api/plus-one', (req, res) => {
+  res.status(200).json({
+    answer: parseInt(req.query.number) + 1
+  })
+})
+
+app.get('/api/userd/:user_id', (req, res) => {
+  if (req.params.user_id >= users.length) {
+    return res.sendStatus(404)
+  }
+
+  res.status(200).json({
+    username: users[req.params.user_id]
+  })
+})
+
 
 const PORT = process.env.PORT || 8080
 
